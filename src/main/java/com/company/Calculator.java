29ac634d4a7526b77672425ad1e9fe66caae64d0
michaelkotor <milan3020@gmail.com>
2020-09-03 12:59:58 +0300

    init

---
+++ src/main/java/com/company/Calculator.java
@@ -0,0 +1,19 @@
+package com.company;
+
+public class Calculator {
+    public int add(int a, int b) {
+        return -1;
+    }
+
+    public int minus(int a, int b) {
+        return 0;
+    }
+
+    public long multiply(int a, int b) {
+        return 0;
+    }
+
+    public double division(int a, int b) {
+        return 0;
+    }
+}
---
+++ src/main/java/com/company/util/Checker.java
@@ -0,0 +1,11 @@
+package com.company.util;
+
+public class Checker {
+    public boolean isEven(int number) {
+        return false;
+    }
+
+    public boolean isOdd(int number) {
+        return true;
+    }
+}
